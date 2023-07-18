ba23dee4b4988ce414b0f115b6d4442bb857d161
ilosemymind <61050909+ilosemymind@users.noreply.github.com>
2023-07-19 00:39:48 +0200

    Add embla carousel

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,1 +1,35 @@
-<template>Start page</template>
+<template>
+	<div>
+		<div class="embla" ref="emblaNode">
+			<div class="embla__container">
+				<div class="embla__slide">Slide 1</div>
+				<div class="embla__slide">Slide 2</div>
+				<div class="embla__slide">Slide 3</div>
+			</div>
+		</div>
+
+		<button @click="emblaApi?.scrollPrev">&lt;</button>
+		<button @click="emblaApi?.scrollNext">></button>
+	</div>
+</template>
+
+<script setup lang="ts">
+import emblaCarouselVue from 'embla-carousel-vue';
+
+const [ emblaNode, emblaApi ] = emblaCarouselVue({ loop: true });
+</script>
+
+<style scoped>
+.embla {
+	overflow: hidden;
+}
+
+.embla__container {
+	display: flex;
+}
+
+.embla__slide {
+	flex: 0 0 100%;
+	min-width: 0;
+}
+</style>
