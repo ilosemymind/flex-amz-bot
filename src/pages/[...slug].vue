e2efce1f6eb6811e0241ff3a3923e6060eaa9344
ilosemymind <61050909+ilosemymind@users.noreply.github.com>
2023-08-12 20:49:41 +0200

    Update pages slug

--- src/pages/[...slug].vue
+++ src/pages/[...slug].vue
@@ -1,1 +1,11 @@
-<template>404</template>
+<template>
+	<MainLayout class="flex justify-center items-center">
+		<span class="text-3xl tex-white">
+			404
+		</span>
+	</MainLayout>
+</template>
+
+<script setup lang="ts">
+import MainLayout from '@/components/layouts/MainLayout.vue';
+</script>
